b8da0cb06336feb3b3ae145039ca31d651717b82
Paul Gathondu <aulgondu@gmail.com>
2023-05-02 18:13:50 -0700

    env

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -9,19 +9,19 @@ import 'firebase/compat/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAcQBYk4MRummz0S1XU4Cu8IRz5jp84B88",
-    authDomain: "website1-92e89.firebaseapp.com",
-    projectId: "website1-92e89",
-    storageBucket: "website1-92e89.appspot.com",
-    messagingSenderId: "134778940064",
-    appId: "1:134778940064:web:bac0e1dbfe3c4ac8ddf7c5",
-    measurementId: "G-MY61ZM3NE2"
-  };
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
+};
+
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
-
-export { db, auth, };
+export { db, auth };
